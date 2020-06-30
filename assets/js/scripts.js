c5a9d23f83e5f61968898cef8dfc2b1961e2781c
Dhruv Ahuja <dhruvahuja2301@gmail.com>
2020-06-30 16:13:57 +0530

    plain html completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,20 @@
-$("li").on("click", function() {
+$("ul").on("click", "li", function() {
 	$(this).toggleClass("completed");
+});
+
+$("ul").on("click", ".delete", function(event){
+	// const x =confirm("Are you sure you want to delete it");
+	// if(x) {
+		$(this).parent().fadeOut(function(){
+			$(this).remove();		
+		});
+		event.stopPropagation();	
+	// }	
+});
+$("input[type='text']").on("keypress",function(event){
+	if(event.which === 13) {
+		const newTodo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span class='delete'>X</span> "+newTodo+"</li>");
+	}
 });
